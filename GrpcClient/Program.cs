8aded7e744c625ca44c7f5ed5a8154056652d018
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-03-04 16:04:06 +0100

    Day 3

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -11,18 +11,29 @@ namespace GrpcClient
         {
             Console.WriteLine("Hello World!");
 
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Virus.Api.VirusService.VirusServiceClient(channel);
 
-            AddInfectedRequest request = new AddInfectedRequest
+            while (true)
             {
-                Confirmed = 1,
-                Country = "Poland",
-                Latitude = 52.45f,
-                Longitude = 28.43f
-            };
+                AddInfectedRequest request = new AddInfectedRequest
+                {
+                    Confirmed = 1,
+                    Country = "Poland",
+                    Latitude = 52.45f,
+                    Longitude = 28.43f
+                };
 
-            await client.AddInfectedAsync(request);
+                var response = await client.AddInfectedAsync(request);
+
+                Console.WriteLine($"Is confirmed = {response.IsConfirmed}");
+
+                // await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+
+            
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
---
+++ GrpcService/Services/MyVirusService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Virus.Api;
+
+namespace GrpcService.Services
+{
+    public class MyVirusService : Virus.Api.VirusService.VirusServiceBase
+    {
+        private readonly ILogger<MyVirusService> logger;
+
+        public MyVirusService(ILogger<MyVirusService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task<AddInfectedResponse> AddInfected(AddInfectedRequest request, ServerCallContext context)
+        {
+            logger.LogInformation($"{request.Country} {request.Confirmed} {request.Latitude} {request.Longitude}");
+
+            var response = new AddInfectedResponse { IsConfirmed = true };
+
+            return Task.FromResult(response);
+        }
+    }
+}
