daf0b081baab6cb418484908cf113ff13ebba3f5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-03-04 15:41:30 +0100

    gRPC client

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using Virus.Api;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Virus.Api.VirusService.VirusServiceClient(channel);
+
+            AddInfectedRequest request = new AddInfectedRequest
+            {
+                Confirmed = 1,
+                Country = "Poland",
+                Latitude = 52.45f,
+                Longitude = 28.43f
+            };
+
+            await client.AddInfectedAsync(request);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
